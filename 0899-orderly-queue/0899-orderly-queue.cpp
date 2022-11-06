8d04f80afef646a449587a762c6a7516840ece5c
Kshitiz Pandey <89742928+kshitiz-access@users.noreply.github.com>
2022-11-06 10:03:43 +0530

    Time: 4 ms (84.48%), Space: 6.4 MB (85.06%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+         if (k == 1) {
+            s.reserve(size(s) * 2);
+            string_view ans{s};
+            s += s;
+            string_view sv{s};
+            for (int i = 1; i < size(ans); ++i)
+                ans = min(ans, sv.substr(i, size(ans)));
+
+            return string{ans};
+        }
+        
+        sort(begin(s), end(s));
+        return s;
+    }
+};
