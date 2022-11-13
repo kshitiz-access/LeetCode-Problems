9f38114748ea0b4fe562380b7eeb1447f5a8bf8c
Kshitiz Pandey <89742928+kshitiz-access@users.noreply.github.com>
2022-11-13 17:36:16 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -35,8 +35,40 @@ bool f(int i,int j,string &s1,string &s2, vector<vector<int>> &dp)
     {
         int n=s1.size();
         int m=s2.size();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        return f(n,m,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        // return f(n,m,s1,s2,dp);
+        dp[0][0]=1;
+        for(int j=1;j<=m;j++) dp[0][j]=0;
+        for(int i=1;i<=n;i++)
+        {
+            int f=1;
+             for(int k=1;k<=i;k++)
+            {
+                if(s1[k-1] != '*')
+                {
+                    f=0;
+                    break;
+                }
+            }
+            dp[i][0]=f;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1] == s2[j-1] || s1[i-1] == '?')
+                     dp[i][j]= dp[i-1][j-1];
+                
+                else if(s1[i-1] == '*')
+                {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else
+                    dp[i][j] = 0;
+            }
+        }
+        return dp[n][m];
     }
 };
 
