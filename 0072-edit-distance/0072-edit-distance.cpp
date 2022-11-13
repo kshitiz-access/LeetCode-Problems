3fa7515d59203f7f025bd3127a6347190a7efb17
Kshitiz Pandey <89742928+kshitiz-access@users.noreply.github.com>
2022-11-13 11:37:20 +0530

    Time: 28 ms (49.45%), Space: 9 MB (60.43%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -18,9 +18,24 @@ public:
         int n=s1.size();
         int m=s2.size();
         
-        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        return f(n,m,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        // return f(n,m,s1,s2,dp);
         
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int j=0;j<=m;j++) dp[0][j]=j;
         
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]== s2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                {
+                    dp[i][j] = min(1+ dp[i-1][j-1],min(1 + dp[i-1][j],1+dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
